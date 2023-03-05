af53d83f5147382e667239f6d31c4e59f000e7f0
Alexandru Leorda <alexandru.leorda@gmail.com>
2023-03-05 10:22:23 +0100

    add app_name to api.urls

--- app/api/urls.py
+++ app/api/urls.py
@@ -13,5 +13,5 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
+app_name = "api"
 urlpatterns = []
