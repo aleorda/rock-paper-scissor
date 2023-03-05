cdb8956fe0e471fbab1645f7b42c5011fdee5f73
Alexandru Leorda <alexandru.leorda@gmail.com>
2023-03-05 10:43:42 +0100

    added more tests

--- app/api/tests.py
+++ app/api/tests.py
@@ -9,3 +9,21 @@ def test_play_endpoint(client):
         "result": "win",
         "computer": "scissors",
     }
+
+
+def test_play_endpoint__invalid_action(client):
+    response = client.post(reverse("api:play"), json={"action": "invalid"})
+    assert response.status_code == 400
+    assert response.json() == {"action": ["Invalid action"]}
+
+
+def test_play_endpoint__no_action(client):
+    response = client.post(reverse("api:play"))
+    assert response.status_code == 400
+    assert response.json() == {"action": ["This field is required."]}
+
+
+def test_play_endpoint__get_method(client):
+    response = client.get(reverse("api:play"))
+    assert response.status_code == 405
+    assert response.json() == {"detail": 'Method "GET" not allowed.'}
--- app/api/views.py
+++ app/api/views.py
@@ -1,5 +1,7 @@
 from django.http import JsonResponse
+from rest_framework.decorators import api_view
 
 
+@api_view(["POST"])
 def play(request):
     return JsonResponse({"action": "rock", "result": "win", "computer": "scissors"})
