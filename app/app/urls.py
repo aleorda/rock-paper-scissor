d6a8499612db9473d02116cee04de222453bcba6
Alexandru Leorda <alexandru.leorda@gmail.com>
2023-03-05 10:24:25 +0100

    add ping endpoint

--- app/app/urls.py
+++ app/app/urls.py
@@ -14,10 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path, include
 from api import urls as api_urls
 
+
+def ping(request):
+    return HttpResponse("pong")
+
+
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("ping/", ping),
     path(r"api/", include(api_urls, namespace="api")),
 ]
