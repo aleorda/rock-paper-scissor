7f843dd7f14f406afff5661fdf5f6938d49c1919
Alexandru Leorda <alexandru.leorda@gmail.com>
2023-03-06 10:15:52 +0100

    black refactor

--- app/frontend/management/commands/test.py
+++ app/frontend/management/commands/test.py
@@ -8,4 +8,4 @@ logger = logging.getLogger(__name__)
 
 class Command(test.Command):
     def handle(self, *args, **options):
-        logger.error('Use pytest command instead of manage.py test')
+        logger.error("Use pytest command instead of manage.py test")
